243d1e6a5dbbc9bdd8fe418ca29bfe5735bb2eb0
Rutemberg <rutembergbarbosa@gmail.com>
2019-03-27 11:49:01 -0300

    Object

---
+++ object.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = "Celular"
+prod1.preco = 9650.0
+prod1['Desconto Legal'] = 0.40 //evitar atributos com espaco
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.50
+}
+
+console.log(prod2)
