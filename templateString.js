6a7b72e9a14340ffa43e6b8e43af429ae71e7eb2
Rutemberg <rutembergbarbosa@gmail.com>
2019-03-27 09:57:25 -0300

    Template string

---
+++ templateString.js
@@ -0,0 +1,11 @@
+const name = "Rutemberg"
+const concatenacao = 'olá ' + name + '!'
+const template = `Olá ${name} !` //crase
+
+console.log(template)
+
+// expressoes ...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei ... ${up('cuidado')}`)
