8acad4181bc64dfea6ffc570dd797e0272b595c0
Rutemberg <rutembergbarbosa@gmail.com>
2019-04-03 10:32:55 -0300

    Foreach

---
+++ Array/foreach.js
@@ -0,0 +1,19 @@
+let aprovados = ["Aldo", "Daniel", "Ana", "Lacuna"]
+
+aprovados.forEach(function (value, key) {
+    console.log(`${key}) ${value}`)
+})
+
+aprovados.forEach(value => console.log(value))
+
+Array.prototype.forEach2 = function (callback) {
+    for (let index = 0; index < this.length; index++) {
+        callback(this[index], index, this)
+    }
+}
+
+aprovados = ["Aldo", "Daniel", "Ana", "Lacuna"]
+
+aprovados.forEach2(function (value, key) {
+    console.log(`${key}) ${value}`)
+})
