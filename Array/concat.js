d0a6342bd750f8890d97a710ae9b44b8f0c1dafa
Rutemberg <rutembergbarbosa@gmail.com>
2019-04-03 16:13:44 -0300

    concat

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const filhos = ["Wei", "Braiz", "Swar"]
+const filhas = ["Weia", "Braiza", "Swara"]
+const todos = filhos.concat(filhas, "Trabuco")
+
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
