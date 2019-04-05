f0041541aa6ab3aa2f2c8c2f4e63fab2ff112e21
Rutemberg <rutembergbarbosa@gmail.com>
2019-04-05 10:10:22 -0300

    Global

---
+++ Node/golbal.js
@@ -0,0 +1,8 @@
+// console.log(global)
+
+global.minhaFunc = Object.freeze({
+    saudacao() {
+        return "Estou em todos os lugares"
+    },
+    nome: "Funcao"
+})
---
+++ Node/golbalCliente.js
@@ -0,0 +1,5 @@
+require("./golbal")
+console.log(minhaFunc.saudacao())
+
+minhaFunc.nome = "Carlos"
+console.log(minhaFunc.nome)
