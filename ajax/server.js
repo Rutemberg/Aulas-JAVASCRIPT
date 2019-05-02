ca82aee5946a1f088303b129c3b995e2d6b3b813
Rutemberg <rutembergbarbosa@gmail.com>
2019-05-02 12:16:23 -0300

    Upload com carregamento em ajax

--- ajax/server.js
+++ ajax/server.js
@@ -6,4 +6,27 @@ app.use(express.static(".")); // provê os conteudos estaticos (importante)
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, "./upload"); // pasta
+  },
+  filename: function(req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`); // nome do arquivo
+  }
+});
+
+const upload = multer({ storage }).single("arquivo"); // nome do input a receber 
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      return res.end("Ocorreu um erro.");
+    }
+
+    res.end("Concluido com sucesso!");
+  });
+});
+
 app.listen(8080, () => console.log("Executando..."));
