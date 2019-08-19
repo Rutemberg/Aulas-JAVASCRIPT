4d6cd2abb5256e227d9e9776c509445b872d2010
Rutemberg <rutembergbarbosa@gmail.com>
2019-08-19 16:21:05 -0300

    React: componentes, props e estado

--- react/exercicios/src/componentes/Multiplos.jsx
+++ react/exercicios/src/componentes/Multiplos.jsx
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
 
-export const BoaTarde = props => <h1>Boa Tarde {props.nome}</h1>
-export const BoaNoite = props => <h1>Boa Noite {props.nome}</h1>
+const BoaTarde = props => <h1>Boa Tarde {props.nome}</h1>;
+const BoaNoite = props => <h1>Boa Noite {props.nome}</h1>;
+
+export { BoaTarde, BoaNoite };
+export default { BoaTarde, BoaNoite };
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import BomDia from './componentes/BomDia'
+// import Multi, { BoaTarde } from "./componentes/Multiplos";
+import Saudacao from "./componentes/Saudacao";
 
-ReactDOM.render(<BomDia nome="Guilherme" idade={10}/>, document.getElementById('root'))
+// ReactDOM.render(
+//   <div>
+//     <BoaTarde nome="Ana" />
+//     <Multi.BoaNoite nome="Bia" />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+ReactDOM.render(
+  <div>
+    <Saudacao tipo='Bom dia' nome="Ana" />
+  </div>,
+  document.getElementById("root")
+);
