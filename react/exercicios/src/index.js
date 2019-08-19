f1ce3d74cdc684db1e4a3552de3cec79a3a9d537
Rutemberg <rutembergbarbosa@gmail.com>
2019-08-19 17:31:51 -0300

    React: Pai e filho usando clone e herdando propriedades

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 // import Multi, { BoaTarde } from "./componentes/Multiplos";
 // import Saudacao from "./componentes/Saudacao";
 import Pai from "./componentes/Pai";
+import Filho from "./componentes/Filho";
 
 // ReactDOM.render(
 //   <div>
@@ -22,7 +23,11 @@ import Pai from "./componentes/Pai";
 
 ReactDOM.render(
   <div>
-    <Pai nome="Julio" sobrenome="Barboza" />
+    <Pai nome="Julio" sobrenome="Barboza">
+        <Filho nome='Paulo' ></Filho>
+        <Filho nome='Pedro' ></Filho>
+        <Filho nome='Carla' ></Filho>
+    </Pai>
   </div>,
   document.getElementById("root")
 );
