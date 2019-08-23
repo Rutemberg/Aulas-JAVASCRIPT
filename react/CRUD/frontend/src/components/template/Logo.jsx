bbf3b673407f71bb28efb64d6337caa5a0e7e5b6
Rutemberg <rutembergbarbosa@gmail.com>
2019-08-23 17:15:14 -0300

    React: tabela de usuarios

--- react/CRUD/frontend/src/components/template/Logo.jsx
+++ react/CRUD/frontend/src/components/template/Logo.jsx
@@ -1,10 +1,12 @@
-import './Logo.css'
-import logo from '../../assets/img/logo.png'
-import React from 'react'
+import "./Logo.css";
+import logo from "../../assets/img/logo.png";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default props => 
-    <aside className='logo'>
-        <a href="/" className="logo">
-            <img src={logo} alt="logo"/>
-        </a>
-    </aside>
+export default props => (
+  <aside className="logo">
+    <Link to="/" className="logo">
+      <img src={logo} alt="logo" />
+    </Link>
+  </aside>
+);
