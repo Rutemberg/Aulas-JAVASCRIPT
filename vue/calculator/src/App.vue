e30ce722df3effd4e68c396a914df1bc8d9d336d
Rutemberg <rutembergbarbosa@gmail.com>
2019-09-06 17:10:37 -0300

    Vue: Projeto Calculator

---
+++ vue/calculator/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>Olá Vue</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+
+import Calculator from "./main/Calculator"
+
+export default {
+
+    components: {Calculator}
+
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", "monospace";
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: white;
+  background: #0f0c29; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #24243e,
+    #302b63,
+    #0f0c29
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #24243e,
+    #302b63,
+    #0f0c29
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
